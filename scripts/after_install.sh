0ffdd80e7ba4a76c213203958d2afab7c928161d
zeMirco <mirco.zeiss@gmail.com>
2014-11-21 09:19:23 +0100

    run couchdb automatically on reboot

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -14,3 +14,9 @@ chmod 0770 /usr/local/etc/couchdb
 chmod 0770 /usr/local/var/lib/couchdb
 chmod 0770 /usr/local/var/log/couchdb
 chmod 0770 /usr/local/var/run/couchdb
+
+# create symbolic link between files
+sudo ln -s /usr/local/etc/init.d/couchdb  /etc/init.d
+
+# install CouchDB as a service and allow it to start on boot
+sudo update-rc.d couchdb defaults
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# start couchdb as couchdb user
-# sudo -i -u couchdb couchdb
-
 # run as deamon
 sudo /usr/local/etc/init.d/couchdb start
